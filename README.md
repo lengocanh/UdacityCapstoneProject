d829d284460abdc3f40911848bb8104fdc56babb
LAPTOP-K91LEKF1\admin <anhln@fsoft.com.vn>
2024-09-05 09:41:51 +0800

    Project Completed

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# UdacityCapstoneProject
-Udacity Capstone Project
+### Table of Contents
+
+1. [Installation](#installation)
+2. [Project Motivation](#motivation)
+3. [File Descriptions](#files)
+4. [Results](#results)
+5. [Licensing, Authors, and Acknowledgements](#licensing)
+
+## Installation <a name="installation"></a>
+
+This is the python scripts that can be run with Python versions 3.*<br>
+Makesure you have following packages installed:
+1. flask
+2. OpenCV
+3. ResNet50
+
+## Project Motivation<a name="motivation"></a>
+
+For this project, I build the web application which can predict the dog breed.<br>
+The application accepts any user-supplied image as input.  <br>
+If a dog is detected in the image, it will provide an estimate of the dog's breed.  <br>
+If a human is detected, it will provide an estimate of the dog breed that is most resembling.<br>
+
+## File Descriptions <a name="files"></a>
+
+#### dog_app.ipynb
+This notebook walks you through one of the most popular Udacity projects across machine learning and artificial intellegence nanodegree programs.<br>
+You maynot run somepart of this notebook in your local computer as the script require the images files and other resources.
+
+#### dog_app-Working.ipynb
+Original detect dogs using ResNet50 was not working, I created it to investigated the issue
+
+#### run.py
+1. Run the following command in the app's directory to run your web app.
+    `python run.py`
+
+2. Go to http://0.0.0.0:3001/
+
+#### master.html
+The html template for home page when user access the web application
+
+#### upload_form.html
+The html template to display predicted dog breed after users choose the image and click on "Clasify Dog"
+
+#### dog_breed.txt
+List of 133 dog breed names the application can casify
+
+#### extract_bottleneck_features.py
+Extract output of bottleneck features
+
+## Results<a name="results"></a>
+1. The web app can predict dog breed from image using InceptionV3
+2. I went through the process to build CNN architecture from scratch and using Transfer Learning to clasify dog breeds
+3. Using Transfer Learning make the algorithm much more accurate and reduce a lot of training time.
+4. Explored pre-trained model OpenCV, ResNet50, VGG16, VGG19 and InceptionV3
+5. Have fun with dog breed classification app
+
+## Licensing, Authors, Acknowledgements<a name="licensing"></a>
+Feel free to use the code here as you would like! <br>
--- app/run.py
+++ app/run.py
@@ -1,6 +1,3 @@
-import pandas as pd
-
-
 from flask import Flask
 from flask import render_template, request
 
@@ -12,25 +9,27 @@ import numpy as np
 import cv2
 from keras.applications.resnet50 import ResNet50
 from keras.preprocessing import image  
-from keras.applications.resnet50 import preprocess_input, decode_predictions                
-from tqdm import tqdm
+from keras.applications.resnet50 import preprocess_input                
 
-from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
-from keras.layers import Dropout, Flatten, Dense
+from keras.layers import GlobalAveragePooling2D
+from keras.layers import Dense
 from keras.models import Sequential
 
 
 app = Flask(__name__)
 
+# Get dog breed names from file
 dog_names = []
 with open('../dog_breed.txt', 'r') as file:
     dog_names = [line.strip() for line in file.readlines()]
 
-# load model
+# load model for face detect
 face_cascade = cv2.CascadeClassifier('../haarcascades/haarcascade_frontalface_alt.xml')
-# define ResNet50 model
+
+# define ResNet50 model for dog detect
 ResNet50_model = ResNet50(weights='imagenet')
 
+# define model architecture and load weights of dog breed detect
 InceptionV3_model = Sequential()
 InceptionV3_model.add(GlobalAveragePooling2D(input_shape=(5, 5, 2048)))
 InceptionV3_model.add(Dense(133, activation='softmax'))
@@ -40,15 +39,30 @@ def extract_InceptionV3(tensor):
 	from keras.applications.inception_v3 import InceptionV3, preprocess_input
 	return InceptionV3(weights='imagenet', include_top=False).predict(preprocess_input(tensor))
 
-# returns "True" if face is detected in image stored at img_path
 def face_detector(file):
+    """Detect faces in a image using OpenCV
+
+    Args:
+    file: image file that user uploaded
+
+    Returns:
+    returns "True" if human face is detected in image
+    """
     file_bytes = np.frombuffer(file, np.uint8)
     img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray)
     return len(faces) > 0
 
-def path_to_tensor(file):
+def file_to_tensor(file):
+    """convert image file to array
+
+    Args:
+    file: image file that user uploaded
+
+    Returns:
+    returns array of the image that can be consumed by the model
+    """
     try:
         img = image.load_img(BytesIO(file), target_size=(224, 224))
         x = image.img_to_array(img)
@@ -59,26 +73,61 @@ def path_to_tensor(file):
         return None
 
 def ResNet50_predict_labels(file):
-    # returns prediction vector for image located at img_path
-    img = preprocess_input(path_to_tensor(file))
+    """Get the max value in predtion vector
+
+    Args:
+    file: image file that user uploaded
+
+    Returns:
+    returns max value in prediction vector, present the main content of the image
+    """
+    # returns prediction vector for image located at img file
+    img = preprocess_input(file_to_tensor(file))
     return np.argmax(ResNet50_model.predict(img))
 
-### returns "True" if a dog is detected in the image stored at img_path
 def dog_detector(file):
+    """Detect if img file has a dog
+
+    Args:
+    file: image file that user uploaded
+
+    Returns:
+    returns "True" if a dog is detected in the image stored at img file
+    """
     prediction = ResNet50_predict_labels(file)
     return ((prediction <= 268) & (prediction >= 151)) 
 
 ### TODO: Write a function that takes a path to an image as input
 ### and returns the dog breed that is predicted by the model.
 def InceptionV3_predict_breed(file):
+    """Detect dog breed in the img file
+    if the img file has not dog, it will provide an estimate of the dog breed that is most resembling.
+
+    Args:
+    file: image file that user uploaded
+
+    Returns:
+    returns: string dog breed name
+    """
     # extract bottleneck features
-    bottleneck_feature = extract_InceptionV3(path_to_tensor(file))
+    bottleneck_feature = extract_InceptionV3(file_to_tensor(file))
     # obtain predicted vector
     predicted_vector = InceptionV3_model.predict(bottleneck_feature)
     # return dog breed that is predicted by the model
     return dog_names[np.argmax(predicted_vector)]
 
 def pedict_dog_breed(file):
+    """Determines whether the image contains a human, dog, or neither.  Then,
+    - if a dog is detected in the image, return the predicted breed.
+    - if a human is detected in the image, return the resembling dog breed.
+    - if neither is detected in the image, provide output that indicates an error.
+
+    Args:
+    file: image file that user uploaded
+
+    Returns:
+    returns: string prediction result
+    """
     is_human = face_detector(file)# 11% dog is detected as human
     is_dog = dog_detector(file)
 
@@ -119,7 +168,7 @@ def upload_file():
             message = 'No selected file'
         )
 
-    # Save the file to the designated directory
+    # predict dog breed 
     if file:
         image_data = file.read()
         encoded_img_data = base64.b64encode(image_data).decode('utf-8')
